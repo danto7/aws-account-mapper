45bc1a46d0bba6558bf920d44b3fe24a04e25222
Daniel Jensen <daniel@danto.cc>
2025-03-07 08:58:47 +0100

    make xo happy

--- source/banner.js
+++ source/banner.js
@@ -1,31 +1,33 @@
-import { log, waitFor } from "./utils.js";
-import { get } from "./accounts-storage.js";
+import {log, waitFor} from './utils.js';
+import {get} from './accounts-storage.js';
 
-log("💈 Content script loaded");
+log('💈 Content script loaded');
 
 async function init() {
 	const accounts = await get();
 
 	const accountId = await waitFor(() => {
 		const topMenu = document.querySelector(
-			"div[data-testid=account-detail-menu]",
+			'div[data-testid=account-detail-menu]',
 		);
-		if (!topMenu) return null;
+		if (!topMenu) {
+			return null;
+		}
 
 		return topMenu.textContent.match(/((\d{4}-?){3})/)[1].replaceAll(
-			"-",
-			"",
+			'-',
+			'',
 		);
 	});
 	if (!accountId) {
 		return;
 	}
 
-	const notice = document.createElement("div");
+	const notice = document.createElement('div');
 	notice.textContent = accounts[accountId];
 	document.body.prepend(notice);
-	notice.id = "text-notice";
-	notice.style.border = "2px solid black";
+	notice.id = 'text-notice';
+	notice.style.border = '2px solid black';
 }
 
 init();
